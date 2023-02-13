11c56f388ce16cb5053008f3fcff711f033600c2
= <joshuarose099@gmail.com>
2023-02-13 20:18:33 +1100

    chore: upload local files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Unit Testing
+_a software testing method by which individual units of source code are tested to determine whether they are fit for use._
+## Description
---
+++ tests/context.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+"""
+Allows for path mutability when testing. modules can be then imported as follows:
+
+>>> import src                 
+>>> from src import my_module
+"""
+
+global_path_index: int = 0  # declare path index to insert global path
+sys.path.insert(global_path_index, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
