15cae8319f9146b0ddd00e3d111bcfbd3c9df1a6
= <joshuarose099@gmail.com>
2023-02-13 20:59:29 +1100

    docs: version test docstring

---
+++ src/__main__.py
@@ -0,0 +1,14 @@
+class Version:
+    """ Description: Manages rules of a given version.
+
+    Each version has a syntax which is applied as follows: x.y.z where:
+        - x is major release
+        - y is a feature
+        - z is a patch
+
+    Functionality: The version class is responsible for regulating these
+    changes and incrementing individual versions as set by the rules explained above.
+    """
+
+    def __init__(self):
+        ...
