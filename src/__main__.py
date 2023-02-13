fdec5a56ee4c3821e2e59ff627cef143a61a4e31
= <joshuarose099@gmail.com>
2023-02-13 21:12:28 +1100

    feat: version class object + docstrings

--- src/__main__.py
+++ src/__main__.py
@@ -1,14 +1,24 @@
 class Version:
-    """ Description: Manages rules of a given version.
+    """ Applies versioning rules to a given instance or string
 
-    Each version has a syntax which is applied as follows: x.y.z where:
-        - x is major release
-        - y is a feature
-        - z is a patch
+    If a string is formatted in the proper version, it can also be 
+    used without creating an instance of Version.
+    The version class is responsible for regulating these
+    changes and incrementing individual versions as set by
+    the following formatting conditions:
 
-    Functionality: The version class is responsible for regulating these
-    changes and incrementing individual versions as set by the rules explained above.
+    Each version has a syntax which is applied as follows: x.y.z, see
+    default keyword below. Eg major.minor.patch
     """
 
-    def __init__(self):
-        ...
+    def __init__(self, default: str ="0.1.0"):
+        """Constructor may be called optionally through class dunder methods.
+
+        Args:
+            default (str): Starting version that will then be incremented from.
+
+        Links:
+            https://peps.python.org/pep-0008/#public-and-internal-interfaces
+        """
+        
+        self._default = default
