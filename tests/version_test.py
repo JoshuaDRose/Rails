a718a10340da108c6c603b8c23441859641b3d33
= <joshuarose099@gmail.com>
2023-02-14 13:33:55 +1100

    chore(lint): lint test passes

--- tests/version_test.py
+++ tests/version_test.py
@@ -1,10 +1,4 @@
-import os
-import sys
 import unittest
-
-global_path_index: int = 0  # declare path index to insert global path
-sys.path.insert(global_path_index, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from src.release import Version
 
 
