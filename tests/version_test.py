a0f03510e5d4b40e05a9b8bd9755ab1ee5f6feac
= <joshuarose099@gmail.com>
2023-02-14 12:20:41 +1100

    refactor: files

--- tests/version_test.py
+++ tests/version_test.py
@@ -1,5 +1,11 @@
+import os
+import sys
 import unittest
 import src.release.Version
+
+global_path_index: int = 0  # declare path index to insert global path
+sys.path.insert(global_path_index, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 
 class TestVersion(unttest.TestCase):
