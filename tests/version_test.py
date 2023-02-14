2bca39042817acd67a04253cb5aa4138dbce34a5
= <joshuarose099@gmail.com>
2023-02-14 12:29:05 +1100

    Tests for both absolute args and pointers for versioning

--- tests/version_test.py
+++ tests/version_test.py
@@ -1,19 +1,23 @@
 import os
 import sys
 import unittest
-import src.release.Version
 
 global_path_index: int = 0  # declare path index to insert global path
 sys.path.insert(global_path_index, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from src.release import Version
 
 
-class TestVersion(unttest.TestCase):
-    self.version = Version()
+class TestVersion(unittest.TestCase):
+    version = Version()
 
-    def __init__(self):
-        super().__init__()
+    def test_default_pointers(self):
+        """ Test default can handle version string """
+        self.assertGreaterEqual(TestVersion.version.count('.'), 2)
 
-    def test_default(self):
-        """ Ensure version keyword argument's within bounds """
-        self.assertTrue(all([True if n else False for n in Version.trisect(self.default)]))
+    def test_default_abs(self):
+        """ Test version keyword argument's within bounds """
+        self.assertTrue(all([True if n else False for n in Version.trisect(TestVersion.version.default)]))
+
+if __name__ == "__main__":
+    unittest.main()
